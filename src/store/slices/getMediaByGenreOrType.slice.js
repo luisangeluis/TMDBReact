8036f8d221bfbb8128dabdbb795599b33253cae6
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2022-10-06 17:45:10 -0500

    Adding functions for entrar to the genres views

--- src/store/slices/getMediaByGenreOrType.slice.js
+++ src/store/slices/getMediaByGenreOrType.slice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const getMediaByGenre = createSlice({
-  name: 'getMediaByGenre',
+export const getMediaByGenreOrType = createSlice({
+  name: 'getMediaByGenreOrType',
   initialState: null,
   reducers: {
     setMediaByGenre: (state, action) => {
@@ -10,8 +10,8 @@ export const getMediaByGenre = createSlice({
   },
 });
 
-export const { setMediaByGenre } = getMediaByGenre.actions;
-export default getMediaByGenre.reducer;
+export const { setMediaByGenre } = getMediaByGenreOrType.actions;
+export default getMediaByGenreOrType.reducer;
 
 export const getMediaByGenre = (mediaType, genreId) => (dispatch) => {
   const key = 'b0dd442bf37e49eecbb517b186e6f5ee';
@@ -19,6 +19,21 @@ export const getMediaByGenre = (mediaType, genreId) => (dispatch) => {
     .get(
       `https://api.themoviedb.org/3/discover/${mediaType}?api_key=${key}&language=en-US&sort_by=popularity.desc&page=1&include_null_first_air_dates=false&with_genres=${genreId}`
     )
+    .then((res) => {
+      dispatch(setMediaByGenre(res.data.genres));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const getPopularMedia = (mediaType, params) => (dispatch) => {
+  params.sort_by = 'popularity.desc';
+  const key = 'b0dd442bf37e49eecbb517b186e6f5ee';
+  params.api_key = key;
+
+  return axios
+    .get(`https://api.themoviedb.org/3/discover/${mediaType}`, params)
     .then((res) => {
       dispatch(setMediaByGenre(res.data.genres));
     })
