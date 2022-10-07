ab763228b4551a50a34792e70fd5bd5ca5856478
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2022-10-07 17:44:33 -0500

    Configuring ruta to the mediaTypeSection

--- src/components/media/MediaByTypeSection.jsx
+++ src/components/media/MediaByTypeSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const MediaByTypeSection = ({ mediaType, query }) => {
+  //TODO make a custom hook by complete the function of this compoonent, to add type and querys
   return (
     <section className="media-section">
       <div className="container"></div>
